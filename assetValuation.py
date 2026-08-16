fd14d73d2bf08d377dc65a55ace32d5c1c6df64d
SahilDevkota <75027114+SahilDevkota@users.noreply.github.com>
2026-08-16 14:33:37 +1000

    changed

--- assetValuation.py
+++ assetValuation.py
@@ -9,5 +9,8 @@ def getAssetValue(symbol: str):
     response = requests.get(f"https://api.twelvedata.com/time_series?symbol={symbol}&interval=1day&outputsize=1&apikey={API_KEY}")
     data = response.json()
     value = data["values"]
+    print("STATUS:", response.status_code)
+    print("RESPONSE:", response.text)
+
     print(response.status_code)
     return value
